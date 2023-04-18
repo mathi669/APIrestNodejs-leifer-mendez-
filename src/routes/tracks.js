8535cde097584ddc1fd36514a2bcfa4d057a83e0
Matias Luarte <e-mluarted@derco.cl>
2023-04-18 13:32:39 -0400

    controllerts init

--- src/routes/tracks.js
+++ src/routes/tracks.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const { getItem, getItems, crearItem, updateItem, deleteItem } = require("../controllers/tracksController");
 const router = express.Router();
 
-router.get('/', (req,res) => {
-
-    const data = ["hola", "mundo"]
-
-    res.send({data})
-});
+router.get('/', getItems);
+router.get('/:id', getItem);
+router.post('/', crearItem);
+router.put('/', updateItem);
+router.delete('/', deleteItem);
 
 
 module.exports = router
