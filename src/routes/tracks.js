46dc7ef9d98a6481ddd4f02ff796594ed05f5750
Matias Luarte <e-mluarted@derco.cl>
2023-05-03 17:30:54 -0400

    validate data

--- src/routes/tracks.js
+++ src/routes/tracks.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { getItem, getItems, crearItem, updateItem, deleteItem } = require("../controllers/tracksController");
+const { validatorCreateItem } = require('../middleware/tracks')
 const router = express.Router();
 
 router.get('/', getItems);
 router.get('/:id', getItem);
-router.post('/', crearItem);
+router.post('/',validatorCreateItem, crearItem);
 router.put('/', updateItem);
 router.delete('/', deleteItem);
 
